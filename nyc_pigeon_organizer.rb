9166807bd91d942f1f754c78811e7181a68c81f0
Spencer Blum <spencerblum@gmail.com>
2020-02-04 18:17:16 +0000

    Automatically backed up by Learn

--- nyc_pigeon_organizer.rb
+++ nyc_pigeon_organizer.rb
@@ -1,26 +1,25 @@
 def nyc_pigeon_organizer(data)
-
   new_hash = {}
-
   data.each do |key, value|
     value.each do |new_value, names|
-      names.each do |name| 
+      names.each do |name|
 
-      if !new_hash[name]
-        new_hash [name] = {}
-      end
+        if !new_hash[name]
+          new_hash[name] = {}
+        end
 
-      if !new_hash[name][key] = []
-        new_hash[name][key] = []
-      end
+        if !new_hash[name][key]
+          new_hash[name][key] = []
+        end
 
-      new_hash[name][key].push(new_value.to_s)
+        new_hash[name][key] << new_value.to_s
 
       end
     end
   end
   new_hash
 end
+
 
 
 # iterate through pigeon data hash
